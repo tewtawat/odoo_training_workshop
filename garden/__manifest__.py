733cc4059f0659e73355fd89be6de5c64d6fcaff
tewtawat <tewtawat@gmail.com>
2020-06-29 12:18:55 +0700

    [ADD] workshop 1 - garden module
    Signed-off-by: tewtawat <tewtawat@gmail.com>

---
+++ garden/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Garden",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Tewtawat C., Trinity Roots",
+    'category': 'Tools',
+    'description': """
+    My Garden
+    """,
+
+    'data': [
+    ],
+
+    'application': True,
+}
